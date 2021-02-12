b6c8d6f8df187a180cf53a3f55f397a82506311c
karlita604 <53234902+karlita604@users.noreply.github.com>
2021-02-11 18:15:15 -0800

    .cpp

---
+++ TripleX.cpp
@@ -0,0 +1,24 @@
+
+
+//we want to be able to make a terminal game 
+//prints out the story at the start of the game
+//that depends on the difficulty
+
+//PlayGame() function
+    //prints out the difficulty
+    // prints out the product of the three numbers
+    //prints out the sum of the three numbers
+    //takes in the three numbers
+        // if correct it prints out congrats and then levels up
+        //if incorrect it prints out oops try again
+
+//the main function 
+    //call PlayGame()
+    //
+    //clear the buffer
+
+
+int main()
+{
+    return 0;
+}
