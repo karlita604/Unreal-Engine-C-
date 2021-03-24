9e3edbff1c9dc4bcfcf182613f72958778044d6b
karlita604 <53234902+karlita604@users.noreply.github.com>
2021-03-24 14:32:47 -0700

    while loop lol

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,50 +1,76 @@
 #include <iostream>
 #include <istream>
+#include <stdlib.h>
 
+std::string space = "\n";
 int level = 1;
+int maxlevel;
+int maxplays=0;
 
-int main()
+void play()
 {
-    std::cout << " \n+ Let there be three unknown integers: x, y and z. \n ";
-    std::cout << std::endl; //ends the line
+    std::cout << "\n\n+ LEVEL: " << level << std::endl;
 
-    std::string space = "\n";
+    //Generating values
+    int FirstValue = rand() % 100;
+    int SecondValue = rand() % 100;
+    int ThirdValue = rand() % 100;
+    int sum = FirstValue + SecondValue + ThirdValue;
+    int product = FirstValue * SecondValue * ThirdValue;
 
-    int FirstValue = 123;
-    int SecondValue = 18;
-    int ThirdValue = 16;
-
-    int sum = FirstValue+SecondValue+ThirdValue;
-    int product = FirstValue*SecondValue*ThirdValue;
-
+    //Printing values
     std::cout << "x + y + z = " << sum << std::endl;
     std::cout << "x * y * z = " << product << std::endl;
     std::cout << space;
 
-    std::cout << "+ To advance to the next level you must enter the correct values of x, y and z. " << std::endl ;       
-    std::cout << "+ Understood?" << std::endl ;  
-    std::cout << "+ Please enter the integers, each separated by a whitespace." << std::endl ;            
-    std::cout << space;
-
+    //User Input
     int FirstInput, SecondInput, ThirdInput;
+    std::cout << "+ Please enter your answer, each value separated by a whitespace.\n";
     std::cin >> FirstInput >> SecondInput >> ThirdInput;
     std::cout << space;
-    std::cout << "+ You entered: \n\n" << "x: " << FirstInput << " y: "<< SecondInput << " z: "<< ThirdInput << space << std::endl;
+    std::cout << "+ YOU ENTERED: "
+              << "x: " << FirstInput << " y: " << SecondInput << " z: " << ThirdInput << space << std::endl;
 
-    int PlayerSum = FirstInput+SecondInput+ThirdInput;
-    int PlayerProduct = FirstInput*SecondInput*ThirdInput;
+    int PlayerSum = FirstInput + SecondInput + ThirdInput;
+    int PlayerProduct = FirstInput * SecondInput * ThirdInput;
 
-    if(PlayerProduct == product && PlayerSum == sum){
-
-        std::cout << "+ CORRECT!" << std::endl;
+    //Checks if the input is correct
+    if (PlayerProduct == product && PlayerSum == sum)
+    {
+        std::cout << " CORRECT!\n";
         level++;
-        std::cout << "+ LEVEL: " << level << std::endl;
-    }
-    else{
+        maxplays++;
 
-        std::cout << "INCORRECT" << std::endl;
-        std::cout << space; 
-        std::cout << "+ LEVEL: " << level << std::endl;
     }
+    else
+    {
+        std::cout << "INCORRECT!\n";
+        maxplays++;
+        std::cout << "+ The answer was: " << FirstValue << " " << SecondValue << " " << ThirdValue << std::endl;
+    }
+}
+
+void intro()
+{
+    std::cout << space;
+    std::cout << " ---------------  WELCOME TO MY GAME! ---------------    \n";
+    std::cout << " + Let there be three unknown integers: x, y and z.\n";
+    std::cout << " + Each value will be between 0 and 100\n";
+    //std::cout << " + I will give you the sum and the product of these values.\n";
+    std::cout << " + To advance to the next level you must enter the correct values of x, y and z. \n";
+    std::cout << space;
+    std::cout << " + How many levels do you want? \n";
+    std::cin >> maxlevel;
+}
+
+int main()
+{
+    intro();
+    //you can only fail 2*maxlevel times
+    while (level < maxlevel && maxplays < 2*maxlevel)
+    {
+        play();
+    }
+    std::cout << "THANKS FOR PLAYING! BYEBYE!";
     return 0;
 }
